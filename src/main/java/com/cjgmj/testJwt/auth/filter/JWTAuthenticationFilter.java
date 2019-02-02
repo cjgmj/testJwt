4081c12999230125dbe9a4f44f9fdf77c0ad5f3d
cjgmj <cjgmj@hotmail.com>
2019-02-02 14:22:34 +0100

    Generar JWT

--- src/main/java/com/cjgmj/testJwt/auth/filter/JWTAuthenticationFilter.java
+++ src/main/java/com/cjgmj/testJwt/auth/filter/JWTAuthenticationFilter.java
@@ -1,5 +1,12 @@
 package com.cjgmj.testJwt.auth.filter;
 
+import java.io.IOException;
+import java.security.Key;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -7,8 +14,15 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -39,6 +53,25 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 		UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, password);
 
 		return authenticationManager.authenticate(authToken);
+	}
+
+	@Override
+	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+			Authentication authResult) throws IOException, ServletException {
+		
+		Key key = Keys.secretKeyFor(SignatureAlgorithm.HS512); 
+		String username = ((User) authResult.getPrincipal()).getUsername();
+		String token = Jwts.builder().setSubject(username).signWith(key).compact();
+		
+		response.addHeader("Authorization", "Bearer ".concat(token));
+		
+		Map<String, Object> body = new HashMap<String, Object>();
+		body.put("token", token);
+		body.put("user", (User) authResult.getPrincipal());
+		
+		response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+		response.setStatus(200);
+		response.setContentType("application/json");
 	}
 
 }
