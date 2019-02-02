b959652a31d6f750e103866eed5fbec34c2d43c8
cjgmj <cjgmj@hotmail.com>
2019-02-02 13:25:07 +0100

    Proyecto base

---
+++ src/test/java/com/cjgmj/testJwt/TestJwtApplicationTests.java
@@ -0,0 +1,17 @@
+package com.cjgmj.testJwt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestJwtApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
