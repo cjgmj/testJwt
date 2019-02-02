56b8eeed96158f1b867fa41896be338df9d7abc5
cjgmj <cjgmj@hotmail.com>
2019-02-02 14:27:58 +0100

    Añadir más datos al token

--- src/main/java/com/cjgmj/testJwt/auth/filter/JWTAuthenticationFilter.java
+++ src/main/java/com/cjgmj/testJwt/auth/filter/JWTAuthenticationFilter.java
@@ -2,6 +2,8 @@ package com.cjgmj.testJwt.auth.filter;
 
 import java.io.IOException;
 import java.security.Key;
+import java.util.Collection;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,12 +16,14 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -58,17 +62,23 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 	@Override
 	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
 			Authentication authResult) throws IOException, ServletException {
-		
-		Key key = Keys.secretKeyFor(SignatureAlgorithm.HS512); 
+
+		Key key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
 		String username = ((User) authResult.getPrincipal()).getUsername();
-		String token = Jwts.builder().setSubject(username).signWith(key).compact();
-		
+		Collection<? extends GrantedAuthority> roles = authResult.getAuthorities();
+
+		Claims claims = Jwts.claims();
+		claims.put("authorities", new ObjectMapper().writeValueAsString(roles));
+
+		String token = Jwts.builder().setClaims(claims).setSubject(username).signWith(key).setIssuedAt(new Date())
+				.setExpiration(new Date(System.currentTimeMillis() + 3600000L)).compact();
+
 		response.addHeader("Authorization", "Bearer ".concat(token));
-		
+
 		Map<String, Object> body = new HashMap<String, Object>();
 		body.put("token", token);
 		body.put("user", (User) authResult.getPrincipal());
-		
+
 		response.getWriter().write(new ObjectMapper().writeValueAsString(body));
 		response.setStatus(200);
 		response.setContentType("application/json");
